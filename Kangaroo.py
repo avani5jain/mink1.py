fe5b42766c7b329207832f047ce6ba71f8b79184
avani5jain <52358306+avani5jain@users.noreply.github.com>
2019-06-30 19:13:25 +0530

    Create Kangaroo.py

---
+++ Kangaroo.py
@@ -0,0 +1,36 @@
+import math
+import os
+import random
+import re
+import sys
+
+# Complete the kangaroo function below.
+def kangaroo(x1, v1, x2, v2):
+    c=x1
+    d=x2
+    for i in range(0,4):
+        c=c+v1
+        d=d+v2
+    if(c==d):
+        return "YES"
+    else:
+        return "NO"
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    x1V1X2V2 = input().split()
+
+    x1 = int(x1V1X2V2[0])
+
+    v1 = int(x1V1X2V2[1])
+
+    x2 = int(x1V1X2V2[2])
+
+    v2 = int(x1V1X2V2[3])
+
+    result = kangaroo(x1, v1, x2, v2)
+
+    fptr.write(result + '\n')
+
+    fptr.close()
